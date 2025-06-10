4121b1f9297ddb73ba2a0d038e10e4b3a7819e2f
Julieta Mariano <julimariano843@gmail.com>
2025-06-10 09:34:47 -0300

    Inicio con el apartado 1 del ejercicio 1

---
+++ ToDo/Tarea.cs
@@ -0,0 +1,10 @@
+namespace EspacioTarea
+{
+    public class Tarea
+    {
+        public int TareaID { get; set; }
+        public string Descripcion { get; set; }
+        public int Duracion { get; set; } // Validar que esté entre 10 y 100
+                                          // Puedes añadir un constructor y métodos auxiliares si lo consideras necesario
+    }
+}
